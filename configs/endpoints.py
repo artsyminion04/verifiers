4f84594071cd2a9e66ee5b18c91dc7fd93e03ba4
Shakti Senthil <artsyminion04@outlook.com>
2025-10-20 17:44:04 +0000

    appworld prompt/rubric changes, sft training script and bash commands

--- configs/endpoints.py
+++ configs/endpoints.py
@@ -99,4 +99,74 @@ ENDPOINTS = {
         "url": "https://api.deepseek.com/v1",
         "key": "DEEPSEEK_API_KEY",
     },
+    "qwen3-1.7b": {
+        "model": "Qwen/Qwen3-1.7B",
+        "url": "http://0.0.0.0:8080/v1",
+        "key": "EMPTY",
+    },
+    "qwen3-4b": {
+        "model": "Qwen/Qwen3-4B",
+        "url": "http://0.0.0.0:8001/v1",
+        "key": "EMPTY",
+    },
+    "qwen3-appworld-sft": {
+        "model": "artsyminion04/Qwen3-4B-Appworld-SFT-432",
+        "url": "http://0.0.0.0:8080/v1",
+        "key": "EMPTY",
+    },
+    "qwen3-a22b-instruct": {
+        "model": "Qwen/Qwen3-235B-A22B-Instruct-2507",
+        "url": "https://api.deepinfra.com/v1/openai",
+        "key": "DEEPINFRA_API_KEY",
+    },
+    "qwen3-235b-i": {
+        "model": "qwen-3-235b-a22b-instruct-2507",
+        "url": "https://api.cerebras.ai/v1",
+        "key": "CEREBRAS_API_KEY",
+    },
+    "qwen3-235b-t": {
+        "model": "qwen-3-235b-a22b-thinking-2507",
+        "url": "https://api.cerebras.ai/v1",
+        "key": "CEREBRAS_API_KEY",
+    },
+    "qwen3-coder": {
+        "model": "Qwen/Qwen3-Coder-480B-A35B-Instruct",
+        "url": "https://api.deepinfra.com/v1/openai",
+        "key": "DEEPINFRA_API_KEY",
+    },
+    "qwen3-32b": {
+        "model": "Qwen/Qwen3-Coder-480B-A35B-Instruct",
+        "url": "https://api.deepinfra.com/v1/openai",
+        "key": "DEEPINFRA_API_KEY",
+    },
+    "glm-4.5": {
+        "model": "zai-org/GLM-4.5",
+        "url": "https://api.deepinfra.com/v1/openai",
+        "key": "DEEPINFRA_API_KEY",
+    },
+    "kimi-k2": {
+        "model": "moonshotai/Kimi-K2-Instruct",
+        "url": "https://api.deepinfra.com/v1/openai",
+        "key": "DEEPINFRA_API_KEY",
+    },
+    "qwen3-a3b-instruct": {
+        "model": "willcb/Qwen3-30B-A3B-Instruct-2507-FP8",
+        "url": "http://0.0.0.0:8000/v1",
+        "key": "EMPTY",
+    },
+    "moonlight": {
+        "model": "moonshotai/Moonlight-16B-A3B-Instruct",
+        "url": "http://0.0.0.0:8000/v1",
+        "key": "EMPTY",
+    },
+    "4b-it": {
+        "model": "google/gemma-3-4b-it",
+        "url": "http://0.0.0.0:8004/v1",
+        "key": "EMPTY",
+    },
+    "devstral": {
+        "model": "mistralai/Devstral-Small-2507",
+        "url": "http://0.0.0.0:8002/v1",
+        "key": "EMPTY",
+    },
 }
